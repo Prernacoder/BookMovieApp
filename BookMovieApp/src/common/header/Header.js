b6c93565a088def0d7eceb7e9fe29329be75cf05
Prernacoder <prernasaxena2@gmail.com>
2023-02-18 17:03:11 +0530

    Adding logo

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Header.css"
+import logo from "../../assets/logo.svg"
 
 export default function Header() {
   return (
     <div className='header1'>
+      <img src={logo} alt="" className="logo1" /> 
       
     </div>
   )
