1a656938801172d9f5c379945104eee497662409
servani93 <servani93@gmail.com>
2023-02-18 11:03:45 +0530

    Heading of UpComming Movies added

---
+++ BookMovieApp/src/screens/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "../../common/header/Header";
+import "./Home.css";
+ 
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <Header/>
+                // to Add Upcoming Movies heading to Home page
+                <h1 className="title">
+                    UpComming Movies
+                </h1>
+            </div>
+            
+        );
+    }
+}
+export default Home;
