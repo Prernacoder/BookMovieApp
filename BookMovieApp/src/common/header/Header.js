40cc8e6ca8a5d28ff52baecf43926e9ae2ef92e2
Prernacoder <prernasaxena2@gmail.com>
2023-02-20 15:14:09 +0530

    ModalFormBasic

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useState } from 'react';
+import ModalDialog from '../ModalDialog';
 import "./Header.css"
 import logo from "../../assets/logo.svg"
 import { Button } from '@material-ui/core';
 
+
 export default function Header() {
   let name = "login";
+
+  // declare a new state variable for modal open
+  const [open, setOpen] = useState(false);
+
+  // function to handle modal open
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // function to handle modal close
+  const handleClose = () => {
+    setOpen(false);
+  };
+ 
   return (
     <div className='header1'>
 
@@ -13,10 +30,12 @@ export default function Header() {
 
       {/* Login ,Logout and BookShow Button */}
       <span className="buttons">
-        <Button id="bookshow" color="primary" variant="contained">BOOK SHOW </Button>
+        <Button id="bookshow" color="primary" variant="contained" >BOOK SHOW </Button>
         &nbsp; &nbsp;
-        <Button id="login" color="default" variant="contained"> {name} </Button></span>
-
+        <Button id="login" color="default" variant="contained" onClick ={handleOpen}> {name} </Button></span>
+        
+        {/*  display the modal and pass props */}
+      <ModalDialog open={open} handleClose={handleClose} />
 
     </div>
   )
