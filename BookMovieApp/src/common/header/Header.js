a76b175f405c1a0cdfcd20d8ce4644968c33df71
servani93 <servani93@gmail.com>
2023-02-20 14:41:56 +0530

    Segrigated Header for home and details page

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -2,22 +2,47 @@ import React from 'react'
 import "./Header.css"
 import logo from "../../assets/logo.svg"
 import { Button } from '@material-ui/core';
+// import BookShow from '../../screens/bookshow/BookShow';
 
-export default function Header() {
+export default function Header(props) {
   let name = "login";
-  return (
-    <div className='header1'>
 
-      {/* adding logo image */}
-      <img src={logo} alt="" className="logo1" />
+  if (props.page === 'detailsPage') {
+    return (
+      <div className='header1'>
 
-      {/* Login ,Logout and BookShow Button */}
-      <span className="buttons">
-        <Button id="bookshow" color="primary" variant="contained">BOOK SHOW </Button>
-        &nbsp; &nbsp;
-        <Button id="login" color="default" variant="contained"> {name} </Button></span>
+        {/* adding logo image */}
+        <img src={logo} alt="" className="logo1" />
+
+        {/* Login ,Logout and BookShow Button */}
+        <span className="buttons" >
+
+          {/* <BookShow/> */}
+          <Button id="bookshow" color="primary" variant="contained">BOOK SHOW </Button>
+          &nbsp; &nbsp;
+
+          <Button id="login" color="default" variant="contained"> {name} </Button>
+
+        </span>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className='header1'>
+
+        {/* adding logo image */}
+        <img src={logo} alt="" className="logo1" />
+
+        {/* Login ,Logout and BookShow Button */}
+        <span className="buttons" >
+
+          <Button id="login" color="default" variant="contained"> {name} </Button>
+
+        </span>
+      </div>
+    )
+  }
 
 
-    </div>
-  )
 }
--- BookMovieApp/src/screens/Controller.js
+++ BookMovieApp/src/screens/Controller.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Home from "../screens/home/Home";
-// import Details from "../screens/details/Details";
+import Details from "../screens/details/Details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "../common/header/Header";
 import BookShow from "../screens/bookshow/BookShow";
 import Confirmation from "../screens/confirmation/Confirmation";
 
@@ -12,16 +11,15 @@ const Controller = () => {
   return (
     <Router>
       <div className="main-container">
-       <Header/>
          <Route
           exact
           path="/"
           render={(props) => <Home {...props} baseUrl={baseUrl} />}
         /> 
-        {/* <Route
+        <Route
           path="/movie/:id"
           render={(props) => <Details {...props} baseUrl={baseUrl} />}
-        /> */}
+        />
        
         <Route
           path="/bookshow/:id"
