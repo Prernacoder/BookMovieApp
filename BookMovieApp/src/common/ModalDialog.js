617c63f9dd26c2a8cdd516da8e0266634f206323
Prernacoder <prernasaxena2@gmail.com>
2023-02-21 08:47:51 +0530

    Login Modal Tabs Change

--- BookMovieApp/src/common/ModalDialog.js
+++ BookMovieApp/src/common/ModalDialog.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
-import { Tab, Tabs, AppBar } from '@material-ui/core';
+import { Tab, Tabs } from '@material-ui/core';
 // import { makeStyles } from '@material-ui/styles';
 // import { formatMs } from '@material-ui/core/styles/transitions';
 
 import LoginForm from './LoginForm';
 
 const ModalDialog = ({ open, handleClose }) => {
-
-
+const [value,setValue]=React.useState(0);
+ const handleTabs =(e,val)=>{
+  console.warn(val);
+  setValue(val);
+ }
   return (
     // props received from App.js
-    <Dialog open={open} onClose={handleClose}>
-      {/* form to be created */}
 
-      <AppBar>
-        <Tabs>
+    <Dialog open={open} onClose={handleClose}>
+      
+
+      {/* Tab changing */}
+
+      
+        <Tabs value={value} onChange ={handleTabs}>
           <Tab label="LOGIN" />
           <Tab label="Register" />
         </Tabs>
-      </AppBar>
+      
+      <TabPanel value={value} index={0}>Item 1 detail</TabPanel>
+      <TabPanel value={value} index={1}>Item 2 details</TabPanel>
+       
       <LoginForm handleClose={handleClose} />
 
     </Dialog>
   );
 };
+
+function TabPanel(props)
+{
+const {children,value,index}=props
+
+  
+  return(
+    <div>
+      {
+        value ===index &&
+        (<h1>{children}</h1>)
+      }
+      
+      
+      </div>
+  )
+}
 
 export default ModalDialog;
